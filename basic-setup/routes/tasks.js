c94fbc81dbf060c8be0061e201ef70c9def76834
Om Balapure <odbalapure@gmail.com>
2021-11-15 21:56:31 +0530

    Basic Express App setup

---
+++ basic-setup/routes/tasks.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deletTask,
+} = require("../controller/tasks");
+
+// Do not require id so we can chain them
+router.route("/").get(getAllTasks).post(createTask);
+router.route("/:id").get(getTask).patch(updateTask).delete(deletTask);
+
+module.exports = router;
